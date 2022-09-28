87b036a5fbcdf0dbb07ad707839b38fe29a10b65
Yuri <yurifdev@gmail.com>
2022-09-28 20:30:19 -0300

    Using setState and prevState to delete items on the list

--- advanced/src/components/ListRender.js
+++ advanced/src/components/ListRender.js
@@ -2,11 +2,23 @@ import React, { useState } from "react";
 
 const ListRender = () => {
   const [list] = useState(["Yuri", "Diego", "Bill"]);
-  const [users] = useState([
+  const [users, setUsers] = useState([
+    // { id: 312321, name: "Matheus", age: 31 },
+    // { id: 23123, name: "João", age: 28 },
+    // { id: 41243, name: "Pedro", age: 44 },
     { id: 1, name: "Matheus", age: 31 },
-    { id: 23123, name: "João", age: 28 },
-    { id: 41243, name: "Pedro", age: 44 },
+    { id: 2, name: "João", age: 28 },
+    { id: 3, name: "Pedro", age: 44 },
   ]);
+
+  const deleteRandom = () => {
+    const randomNumber = Math.floor(Math.random() * 4) //Número de itens da lista
+    setUsers((prevUsers) => { //prevSate é o estado atual, após o método, vira o estado anterior
+      return prevUsers.filter((user) => randomNumber !== user.id)
+    })
+
+  }
+
   return (
     <div>
       <ul>
@@ -18,11 +30,11 @@ const ListRender = () => {
       </ul>
       {/* Em projetos reais, normalmente cada item vem com um ID desse jeito */}
       <ul>
-        {users.map( (user) => (
-            <li key={user.id}>{user.name}</li>
-
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
         ))}
       </ul>
+      <button onClick={deleteRandom}>Delete random user</button>
     </div>
   );
 };
