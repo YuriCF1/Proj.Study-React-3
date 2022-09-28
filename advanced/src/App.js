71ddf37b92ec9655ef0a0cb3afa5ac495bbbcb23
Yuri <yurifdev@gmail.com>
2022-09-28 12:35:42 -0300

    Putting images

--- advanced/src/App.js
+++ advanced/src/App.js
@@ -1,31 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+//Img
+import Rio from './assets/rio.jpg' 
 
 function App() {
   return (
     <div className="App">
       <h1>Avançando</h1>
+      <div>
+        {/* Na pasta public, eu posso acessar os arquivos só usando a barra / */}
+        <img src="/toronto.jfif" alt="Toronto" />
+      </div>
+      <div>
+        {/* Porém,  normalmente fica na pasta assets dentro de src. Tem que importar la em cima */}
+        <img src={Rio} alt="" />
+      </div>
     </div>
   );
 }
 
 export default App;
-
-// return (
-//   <div className="App">
-//     <header className="App-header">
-//       <img src={logo} className="App-logo" alt="logo" />
-//       <p>
-//         Edit <code>src/App.js</code> and save to reload.
-//       </p>
-//       <a
-//         className="App-link"
-//         href="https://reactjs.org"
-//         target="_blank"
-//         rel="noopener noreferrer"
-//       >
-//         Learn React
-//       </a>
-//     </header>
-//   </div>
-// );
