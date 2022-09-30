f7b6b785fa1e9c716a22f808e42eadedb80096cf
Yuri <yurifdev@gmail.com>
2022-09-29 23:53:38 -0300

    Ternary IF

--- advanced/src/components/ConditionalRender.js
+++ advanced/src/components/ConditionalRender.js
@@ -1,20 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const ConditionalRender = () => {
-    const [x] = useState(false)
+  const [x] = useState(false);
 
-    const conso = () => {
-        console.log('É falso');
+  const conso = () => {
+    console.log("É falso");
+  };
 
-    }
-    return (
+  const [name, setName] = useState("João");
+  return (
     <div>
-        <h3>Isso embaixo, será exibido?</h3>
-        {x && <p>Se X for 'true', sim!</p>}
-        {!x && <p>Se X for 'false', sim!</p>}
-        {!x && conso()}
+      <h3>Isso embaixo, será exibido?</h3>
+      {x && <p>Se X for 'true', sim!</p>}
+      {!x && <p>Se X for 'false', sim!</p>}
+      {!x && conso()}
+      <h2>If Ternário</h2>
+      {name === "João" ? (
+        <div>
+          <p>O nome é João</p>
+        </div>
+      ) : (
+        <div>
+          <p>O não é João</p>
+        </div>
+      )}
+      <button onClick={() => setName('Yuri')}>Mudando o nome</button>
     </div>
-  )
-}
+  );
+};
 
-export default ConditionalRender
+export default ConditionalRender;
