188c43d4d7f043b7442ad2d004f2bd8478c55dfa
Yuri <yurifdev@gmail.com>
2022-10-01 13:30:08 -0300

    Using loops

--- advanced/src/App.js
+++ advanced/src/App.js
@@ -11,7 +11,31 @@ import ShowUserName from "./components/ShowUserName";
 
 function App() {
   const name = "Yago";
-  const [userName] = useState("Ygor ")
+  const [userName] = useState("Ygor ");
+
+  const cars = [
+    {
+      id: 1,
+      brand: "Lamborguini",
+      color: "Preta",
+      KM: 0,
+      newCar: true,
+    },
+    {
+      id: 2,
+      brand: "Masserati",
+      color: "Laranja",
+      KM: 100,
+      newCar: true,
+    },
+    {
+      id: 1,
+      brand: "Dodge",
+      color: "Preta",
+      KM: 250,
+      newCar: true,
+    },
+  ];
   return (
     <div className="App">
       <h1>Avançando</h1>
@@ -31,10 +55,16 @@ function App() {
       <ShowUserName name={userName} />
       {/* Destructuring */}
       {/* Número sem ser texto, usa chaves */}
-      <CarDetail brand="Ford" km={1000} color="Azul" new={false}/>
+      <CarDetail brand="Ford" km={1000} color="Azul" new={false} />
       {/* Reaproveitando */}
-      <CarDetail brand="Audi" km={500} color="Branco" new={false}/>
-      <CarDetail color="Roxo" km={700} brand="Fiat" newCar={true}/>
+      <CarDetail brand="Audi" km={500} color="Branco" new={false} />
+      <CarDetail color="Roxo" km={700} brand="Fiat" newCar={true} />
+      {/* Loop em array de objetos */}
+      {/* Lembrando que objeto se passa com parenteses */}
+      {/* Os loops são feitos com .map */}
+      {cars.map((car) => (
+        <CarDetail brand={car.brand} color={car.color} newCar={car.newCar}/>
+      ))}
     </div>
   );
 }
--- advanced/src/components/CarDetail.js
+++ advanced/src/components/CarDetail.js
@@ -4,14 +4,14 @@ const CarDetail = ({ brand, km, color, newCar }) => {
   //Respeitar a ordem(acho q não) e os valores
   return (
     <div>
-      <h2>Detalhes do carro</h2>
+      <h1>Detalhes do carro</h1>
       <ul>
         <li>Marca: {brand}</li>
         <li>KM: {km}</li>
         <li>Cor : {color}</li>
       </ul>
       {/* Condicional, exemplo de aproveitamento */}
-      {newCar && <h1>Este carro é novo</h1>}
+      {newCar && <h5>Este carro é novo</h5>}
     </div>
   );
 };
